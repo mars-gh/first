ea55467b092b503cf47874a25da7dfb496f807e2
mars <maxiaocnjat@gmail.com>
2017-09-06 08:32:03 +0800

    这是一个测试项目。

---
+++ app/src/main/java/com/mars/githtubtest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mars.githtubtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this,"这是一个测试",Toast.LENGTH_SHORT);
+    }
+}
